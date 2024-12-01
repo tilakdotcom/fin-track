a91eb21d9e4290e25172985b3d07dcd21b01c993
tilak-dev <tsingh99991@gmail.com>
2024-12-01 18:14:18 +0530

    get incomesapi called

--- client/src/pages/IncomePage.tsx
+++ client/src/pages/IncomePage.tsx
@@ -1,44 +1,47 @@
 import Card from "@/components/Card";
-import React from "react";
-
+import api from "@/lib/axiousInstance";
+import React, { useEffect, useState } from "react";
+  interface IEnumerable {
+    source: string;
+    amount: number;
+    createdAt: string;
+    category: string;
+    type: string;
+  }
 const Income: React.FC = () => {
-  const expenseData = [
-    {
-      title: "Groceries",
-      amount: 120.5,
-      date: "2024-11-01",
-      category: "Food",
-    },
-    {
-      title: "Electricity Bill",
-      amount: 75.3,
-      date: "2024-11-05",
-      category: "Utilities",
-    },
-    {
-      title: "Groceries",
-      amount: 120.5,
-      date: "2024-11-01",
-      category: "Food",
-    },
-    {
-      title: "Electricity Bill",
-      amount: 75.3,
-      date: "2024-11-05",
-      category: "Utilities",
-    },
-  ];
+  const [data , setData] = useState<IEnumerable[] | null>(null)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [loading,setLoading] = useState<boolean>(false)
 
+
+  const fetchIncomeItem = async()=>{
+    setLoading(true)
+    try {
+      const res = await api.get("/income")
+      if(!res){
+        throw new Error("Error fetching income item")
+      }
+      setData(res.data.data.incomes)
+    } catch (error) {
+      console.error("Error fetching income item", error);
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  useEffect(()=>{
+    fetchIncomeItem()
+  },[])
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <h1 className="text-2xl font-bold text-gray-800 text-center py-3 md:py-5">All Expenses Tracker</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
-        {expenseData.map((expense, index) => (
+        {data && data.map((expense, index) => (
           <Card
             key={index}
-            title={expense.title}
+            title={expense.source}
             amount={expense.amount}
-            date={expense.date}
+            date={expense.createdAt}
             category={expense.category}
             type="income"
           />
