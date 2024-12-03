095bbe3a90f5e355265e140d6724d883059027ce
tilak-dev <tsingh99991@gmail.com>
2024-12-03 16:19:16 +0530

    PORT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ dotEnv.config({
 dbConnect()
  .then(()=>{
      console.log("Connected to the database!");
-     app.listen(process.env.PORT, () => {
+     app.listen(process.env.PORT || 5000, () => {
          console.log(`Server running on port ${process.env.PORT}`);
      });
      app.on("error",(error)=>{
